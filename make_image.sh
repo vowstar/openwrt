153a0f7edd4699bf4c3336fb06fb3546bdf03225
Huang Rui <vowstar@gmail.com>
2021-06-23 19:25:07 +0800

    config.default: fix DNS config and build script
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- make_image.sh
+++ make_image.sh
@@ -2,8 +2,11 @@
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-cp config.default .config
-make olddefconfig
+
+make clean
+cp -f config.default .config
+yes "" | make oldconfig
+make -j $((1+`nproc`)) download
 
 ADMIN="htop"
 BASE="block-mount ca-bundle"
@@ -11,10 +14,14 @@ DOCKER="docker docker-compose"
 HARDWARE="open-vm-tools kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-usb-storage intel-microcode kmod-ixgbe"
 FS="kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 kmod-fs-xfs kmod-fs-nfs"
 LUCI="luci-i18n-base"
-NET="bind-dig bind-host curl ethtool iftop openssh-server wget"
+NET="curl ethtool iftop openssh-server wget"
 SYS="pciutils usbutils"
 TOOLS=""
 UTILS="bash lsof shadow-useradd vim-fuller"
 REMOVE=""
 
-make -j $((1+`nproc`)) PACKAGES="$ADMIN $BASE $DOCKER $HARDWARE $FS $LUCI $NET $SYS $TOOLS $UTILS $REMOVE" V=s
+if [ "x$V" != "x" ]; then
+    make -j $((1+`nproc`)) PACKAGES="$ADMIN $BASE $DOCKER $HARDWARE $FS $LUCI $NET $SYS $TOOLS $UTILS $REMOVE" V=s
+else
+    make -j $((1+`nproc`)) PACKAGES="$ADMIN $BASE $DOCKER $HARDWARE $FS $LUCI $NET $SYS $TOOLS $UTILS $REMOVE"
+fi
