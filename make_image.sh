9d317f7f6671e512182ebf169e21e68fea31390f
Huang Rui <vowstar@gmail.com>
2021-06-23 20:46:50 +0800

    kmod-mlx4-core: enable kernel module
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- make_image.sh
+++ make_image.sh
@@ -11,7 +11,7 @@ make -j $((1+`nproc`)) download
 ADMIN="htop"
 BASE="block-mount ca-bundle"
 DOCKER="docker docker-compose"
-HARDWARE="open-vm-tools kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-usb-storage intel-microcode kmod-ixgbe"
+HARDWARE="open-vm-tools kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-usb-storage intel-microcode kmod-ixgbe kmod-mlx4-core"
 FS="kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 kmod-fs-xfs kmod-fs-nfs"
 LUCI="luci-i18n-base"
 NET="curl ethtool iftop openssh-server wget"
